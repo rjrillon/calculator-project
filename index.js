c666aa1f885674457f6827ed4af1932d53d857a6
Robin <rjrillon97@gmail.com>
2025-05-05 09:17:53 -0700

    Updated boilerplate
    - Added operate function
    - Updated divide to give error
    - Added variables for calculator interface later

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+let operator1, operator2, operand;
+
+function operate(x, op, y) {
+  switch (op) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+  }
+}
+
 function add(x, y) {
   return x + y;
 }
@@ -9,5 +24,8 @@ function multiply(x, y) {
   return x * y;
 }
 function divide(x, y) {
+  if (x === 0 || y === 0) {
+    throw new Error("error cant divide by 0");
+  }
   return x / y;
 }
